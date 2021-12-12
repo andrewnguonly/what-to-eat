aacf08bae48e01a841cb9163e93eb49cf64787f6
Andrew Nguonly <andrewnguonly@gmail.com>
2021-12-12 14:32:07 -0800

    Add constructor to Meal class.

--- src/Meal.ts
+++ src/Meal.ts
@@ -2,6 +2,12 @@ class Meal {
   name: string;
   last_eaten_ts: number;
   eaten_count: number;
+
+  constructor(name, last_eaten_ts, eaten_count) {
+    this.name = name;
+    this.last_eaten_ts = last_eaten_ts;
+    this.eaten_count = eaten_count;
+  }
 }
 
 export default Meal;
