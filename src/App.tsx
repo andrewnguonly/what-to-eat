f408701c80bf1f343f12ca0d746182b3e8d7486d
Andrew Nguonly <andrewnguonly@gmail.com>
2021-12-13 22:27:03 -0800

    Implement and style default list view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text } from 'react-native';
-import getMeals from './Controller';
+import React from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import getMeals from "./Controller";
 
 const styles = StyleSheet.create({
-
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    height: 60,
+    paddingLeft: "5%",
+    paddingRight: "5%",
+  },
+  rowLabel: {
+    color: "gray",
+    fontSize: 20,
+  },
+  rowLabelCol: {
+    alignItems: "flex-end",
+    width: "50%",
+  },
+  rowSeparator: {
+    backgroundColor: "lightgray",
+    height: 1,
+    marginLeft: "5%",
+  },
+  rowTitle: {
+    fontSize: 20,
+  },
+  rowTitleCol: {
+    alignItems: "flex-start",
+    width: "50%",
+  },
   title: {
-    color: 'black',
+    color: "black",
     fontSize: 30,
-    textAlign: 'center',
-  }
+    fontWeight: "100",
+    textAlign: "center",
+  },
 });
 
 const App = () => {
-
   const meals = getMeals();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text style={styles.title}>What to eat?</Text>
       <FlatList
+        ItemSeparatorComponent={() => <View style={styles.rowSeparator} />}
         data={meals}
-        renderItem={({item}) => <Text>{item.name}</Text>}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <View style={styles.rowTitleCol}>
+              <Text style={styles.rowTitle}>{item.name}</Text>
+            </View>
+            <View style={styles.rowLabelCol}>
+              <Text style={styles.rowLabel}>{item.last_eaten_ts}</Text>
+            </View>
+          </View>
+        )}
       />
     </SafeAreaView>
   );
--- src/Controller.ts
+++ src/Controller.ts
@@ -3,11 +3,17 @@ import Meal from "./Meal";
 function getMeals(): Meal[] {
   const meal1 = new Meal();
   meal1.name = "hamburger";
+  meal1.last_eaten_ts = 0;
 
   const meal2 = new Meal();
   meal2.name = "pizza";
+  meal2.last_eaten_ts = 2;
 
-  return new Array(meal1, meal2);
+  const meal3 = new Meal();
+  meal3.name = "tacos";
+  meal3.last_eaten_ts = 1;
+
+  return new Array(meal1, meal2, meal3);
 }
 
 export default getMeals;
