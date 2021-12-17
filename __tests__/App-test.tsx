4c4fd3024bfe57cb9ea49601c86c32801e12fa01
Andrew Nguonly <andrewnguonly@gmail.com>
2021-12-16 22:35:05 -0800

    Fix broken unit test.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,7 +4,12 @@ import App from "../src/App";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
+import TestRenderer from "react-test-renderer";
 
-it("renders correctly", () => {
-  renderer.create(<App />);
+const { act } = TestRenderer;
+
+it("renders correctly", async () => {
+  await act(async () => {
+    renderer.create(<App />);
+  });
 });
