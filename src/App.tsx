b5db7084aa7ee8c4d842e9a761cfdd3013246059
Andrew Nguonly <andrewnguonly@gmail.com>
2021-12-30 09:45:34 -0800

    Refactor UI components. Break up App component into smaller components.

--- src/App.tsx
+++ src/App.tsx
@@ -90,21 +90,14 @@ export const formatTs = (ts: number) => {
   }
 };
 
-const App = () => {
-  const [data, setData] = useState<Meal[]>([]);
+type RefreshDataFunction = () => void;
+
+const TitleBar = ({ refreshData }: { refreshData: RefreshDataFunction }) => {
   const [newMealDialogVisible, setNewMealDialogVisible] = useState(false);
-  const [existingMealDialogVisible, setExistingMealDialogVisible] =
-    useState(false);
   const [newMeal, setNewMeal] = useState("");
-  const [existingMeal, setExistingMeal] = useState("");
 
   const showAddMealDialog = () => {
     setNewMealDialogVisible(true);
-  };
-
-  const showExistingMealDialog = (meal: Meal) => {
-    setExistingMeal(meal.name);
-    setExistingMealDialogVisible(true);
   };
 
   const handleAddMeal = async () => {
@@ -114,6 +107,52 @@ const App = () => {
 
     // refresh meals state data
     refreshData();
+  };
+
+  const handleCancel = () => {
+    setNewMealDialogVisible(false);
+  };
+
+  return (
+    <View style={styles.titleBar}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What to eat?</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable onPress={showAddMealDialog}>
+          <Text style={styles.buttonText}>add meal</Text>
+          <Dialog.Container visible={newMealDialogVisible}>
+            <Dialog.Title>New meal!</Dialog.Title>
+            <Dialog.Description>Enter your meal</Dialog.Description>
+            <Dialog.Input
+              placeholder="e.g. pizza"
+              onChangeText={(meal) => setNewMeal(meal)}
+            />
+            <Dialog.Button label="Cancel" onPress={handleCancel} />
+            <Dialog.Button label="Eat!" onPress={handleAddMeal} />
+          </Dialog.Container>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const MealItem = ({
+  name,
+  last_eaten_ts,
+  refreshData,
+}: {
+  name: string;
+  last_eaten_ts: number;
+  refreshData: RefreshDataFunction;
+}) => {
+  const [existingMealDialogVisible, setExistingMealDialogVisible] =
+    useState(false);
+  const [existingMeal, setExistingMeal] = useState("");
+
+  const showExistingMealDialog = (mealName: string) => {
+    setExistingMeal(mealName);
+    setExistingMealDialogVisible(true);
   };
 
   const handleExistingMeal = async () => {
@@ -126,9 +165,31 @@ const App = () => {
   };
 
   const handleCancel = () => {
-    setNewMealDialogVisible(false);
     setExistingMealDialogVisible(false);
   };
+
+  return (
+    <Pressable onPress={() => showExistingMealDialog(name)}>
+      <View style={styles.row}>
+        <View style={styles.rowTitleCol}>
+          <Text style={styles.rowTitle}>{name}</Text>
+        </View>
+        <View style={styles.rowLabelCol}>
+          <Text style={styles.rowLabel}>{formatTs(last_eaten_ts)}</Text>
+        </View>
+      </View>
+      <Dialog.Container visible={existingMealDialogVisible}>
+        <Dialog.Title>Repeat...</Dialog.Title>
+        <Dialog.Description>Eating {existingMeal} again?</Dialog.Description>
+        <Dialog.Button label="Cancel" onPress={handleCancel} />
+        <Dialog.Button label="Eat!" onPress={handleExistingMeal} />
+      </Dialog.Container>
+    </Pressable>
+  );
+};
+
+const App = () => {
+  const [data, setData] = useState<Meal[]>([]);
 
   const refreshData = () => {
     getMeals()
@@ -143,50 +204,16 @@ const App = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.titleBar}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>What to eat?</Text>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Pressable onPress={showAddMealDialog}>
-            <Text style={styles.buttonText}>add meal</Text>
-            <Dialog.Container visible={newMealDialogVisible}>
-              <Dialog.Title>New meal!</Dialog.Title>
-              <Dialog.Description>Enter your meal</Dialog.Description>
-              <Dialog.Input
-                placeholder="e.g. pizza"
-                onChangeText={(meal) => setNewMeal(meal)}
-              />
-              <Dialog.Button label="Cancel" onPress={handleCancel} />
-              <Dialog.Button label="Eat!" onPress={handleAddMeal} />
-            </Dialog.Container>
-          </Pressable>
-        </View>
-      </View>
+      <TitleBar refreshData={refreshData} />
       <FlatList
         ItemSeparatorComponent={() => <View style={styles.rowSeparator} />}
         data={data}
         renderItem={({ item }) => (
-          <Pressable onPress={() => showExistingMealDialog(item)}>
-            <View style={styles.row}>
-              <View style={styles.rowTitleCol}>
-                <Text style={styles.rowTitle}>{item.name}</Text>
-              </View>
-              <View style={styles.rowLabelCol}>
-                <Text style={styles.rowLabel}>
-                  {formatTs(item.last_eaten_ts)}
-                </Text>
-              </View>
-            </View>
-            <Dialog.Container visible={existingMealDialogVisible}>
-              <Dialog.Title>Repeat...</Dialog.Title>
-              <Dialog.Description>
-                Eating {existingMeal} again?
-              </Dialog.Description>
-              <Dialog.Button label="Cancel" onPress={handleCancel} />
-              <Dialog.Button label="Eat!" onPress={handleExistingMeal} />
-            </Dialog.Container>
-          </Pressable>
+          <MealItem
+            name={item.name}
+            last_eaten_ts={item.last_eaten_ts}
+            refreshData={refreshData}
+          />
         )}
       />
     </SafeAreaView>
